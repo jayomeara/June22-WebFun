7166a659d24791cec9d8fc96fbb066227ee80e63
jayomeara <jay@idlewild.industries>
2022-06-21 21:23:24 -0600

    tuesday EoD

--- assignments/week3/buttonClicker/scripts/scritps.js
+++ assignments/week3/buttonClicker/scripts/scritps.js
@@ -1,3 +1,17 @@
-function example(element) {
-    console.log("element clicked", element);
+// copied from solution for testing
+
+function message() {
+    alert("Ninja was liked!");
+}
+
+function login(element) {
+    if(element.innerText == "Login") {
+        element.innerText = "Logout";
+    } else {
+        element.innerText = "Login";
+    }
+}
+
+function hide(el) {
+    el.remove();
 }
