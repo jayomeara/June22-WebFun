a243695d8067f44e414deecdcb65dc5b778d731a
jayomeara <jay@idlewild.industries>
2022-06-23 16:56:06 -0600

    Thursday Pre-Class

---
+++ assignments/week3/likes/scripts/scripts.js
@@ -0,0 +1,25 @@
+var peppercount = 3
+var countElement3 = document.querySelector("#pepperCount")
+
+function add3() {
+    peppercount ++;
+    countElement3.innerText = peppercount + " Petting(s)";
+    // console.log(brucecount);
+}
+var brucecount = 5
+var countElement2 = document.querySelector("#bruceCount")
+
+function add2() {
+    brucecount ++;
+    countElement2.innerText = brucecount + " Petting(s)";
+    // console.log(brucecount);
+}
+
+var oscarcount = 8
+var countElement = document.querySelector("#oscarCount")
+
+function add1() {
+    oscarcount ++;
+    countElement.innerText = oscarcount + " Petting(s)";
+    // console.log(oscarcount);
+}
