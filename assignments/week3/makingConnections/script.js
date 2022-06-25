dfde06f5b02dca9e5d80f4dd135e898adf2d8875
jayomeara <jay@idlewild.industries>
2022-06-25 16:01:00 -0600

    making connections

--- assignments/week3/makingConnections/script.js
+++ assignments/week3/makingConnections/script.js
@@ -1,1 +1,22 @@
 console.log("page loaded...");
+
+var requestSpan = document.querySelector("#requests");
+var connectionSpan = document.querySelector("#connections");
+var username = document.querySelector("#name");
+
+function changeName(element) {
+    username.innerText = "Jay O'Meara";
+}
+
+function accept(id) {
+    var element = document.querySelector(id);
+    element.remove();
+    requestSpan.innerText--;
+    // connectionSpan.innerText++;
+}
+
+function ignore(id) {
+    var element = document.querySelector(id);
+    element.remove();
+    requestSpan.innerText--;
+}
