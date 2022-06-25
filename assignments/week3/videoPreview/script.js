dceef35d02c412fbae6a34f06294e31ddd6d9347
jayomeara <jay@idlewild.industries>
2022-06-25 14:18:46 -0600

    video page

--- assignments/week3/videoPreview/script.js
+++ assignments/week3/videoPreview/script.js
@@ -2,14 +2,19 @@ console.log("page loaded...");
 
 
 
+// function over(element) {
+//     // element.style.backgroundColor = "lime";
+//     var x = document.getElementById("exampleVideo");
+//     document.getElementById("exampleVideo").play;
+// }
+// function out(element) {
+//     // element.style.backgroundColor = "silver";
+//     var x = document.getElementById("exampleVideo");
+//     document.getElementById("exampleVideo").pause;
+// }
 function over(element) {
-    // element.style.backgroundColor = "lime";
-    var x = document.getElementById("exampleVideo");
-    document.getElementById("exampleVideo").play;
+    element.play();
 }
-    
 function out(element) {
-    // element.style.backgroundColor = "silver";
-    var x = document.getElementById("exampleVideo");
-    document.getElementById("exampleVideo").pause;
+    element.pause();
 }
