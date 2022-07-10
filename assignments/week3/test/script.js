9ebdfa815b79c726fa0787acc4050dc76ac0c31b
jayomeara <jay@idlewild.industries>
2022-07-10 14:55:03 -0600

    js changes

--- assignments/week3/test/script.js
+++ assignments/week3/test/script.js
@@ -1,6 +1,19 @@
-var nameTag = document.querySelector("#name-tag");
+// var nameTag = document.querySelector("#name-tag");
 
-function setName(element) {
-    console.log(element.value);
-    nameSpan.innerText = element.value;
+// function setName(element) {
+//     console.log(element.value);
+//     nameSpan.innerText = element.value;
+// }
+
+function positiveNegative(){
+    var arr = [2,4,6,8,-2,-5,4,9,-60]
+    var positive = []
+    var negative = []
+  for(let i = 0; i< arr.length; i++){
+    if(arr[i]<0){
+      negative.push(arr[i])
+    }
+  }
+  console.log(negative)
 }
+positiveNegative()
